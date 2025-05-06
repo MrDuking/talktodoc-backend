e45a0234662aa6026296e9a3221c4cf62ff0b542
MrDuking <huuduc122003@gmail.com>
2025-05-06 17:07:06 +0700

    update: verify otp

--- src/modules/otp_service/otp.module.ts
+++ src/modules/otp_service/otp.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { OtpService } from './otp.service';
-import { OtpController } from './otp.controller';
-import { EmailOtp, EmailOtpSchema } from './schemas/email-otp.schema';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { OtpService } from './otp.service'
+import { OtpController } from './otp.controller'
+import { EmailOtp, EmailOtpSchema } from './schemas/email-otp.schema'
+import {
+    Doctor,
+    Patient,
+    Employee,
+    DoctorSchema,
+    PatientSchema,
+    EmployeeSchema
+  } from "@modules/user-service/schemas"
+
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: EmailOtp.name, schema: EmailOtpSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: EmailOtp.name, schema: EmailOtpSchema },
+      { name: Doctor.name, schema: DoctorSchema },
+      { name: Patient.name, schema: PatientSchema },
+      { name: Employee.name, schema: EmployeeSchema },
+    ])
+  ],
   controllers: [OtpController],
   providers: [OtpService],
   exports: [OtpService],
